437d76c0c21dbcfac67c9131b694503afddf834c
ysqi <devysq@gmail.com>
2021-07-24 22:38:14 +0800

    demo

---
+++ contracts/Bob.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Created by Flux Team
+
+pragma solidity 0.6.12;
+
+    contract Bob {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+}
