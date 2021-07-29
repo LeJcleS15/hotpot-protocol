e29ef1b5fa16b783af623238c917dbee2ecdc149
peekpi <894646171@qq.com>
2021-07-29 23:47:54 +0800

    add oracle

--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -16,4 +16,12 @@ interface IPriceOracle {
       @return uint256 资产价格（尾数）
      */
     function getPriceMan(address token) external view returns (uint256);
+
+    /**
+      @notice 获取资产的价格
+      @param token 资产
+      @return updateAt 更新时间
+      @return price 价格
+     */
+    function getLastPriceMan(address token) external view returns (uint256 updateAt, uint256 price);
 }
